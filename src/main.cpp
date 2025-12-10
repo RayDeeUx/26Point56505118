0177d8f6b410a750235507fe6c213ce9298a8edc
Erymanthus (RayDeeUx) <raydeeux@gmail.com>
2025-12-10 01:51:26 -0500

    OK THERE WE GO

--- src/main.cpp
+++ src/main.cpp
@@ -23,13 +23,11 @@ class $modify(MyGJRotationControl, GJRotationControl) {
 		m_controlSprite->setPosition(m_controlPosition);
 	}
 	void onCW26Point56505118(CCObject* sender) {
-		if (!sender || !m_delegate) return;
-		log::info("onCW26Point56505118");
+		if (!sender || !m_delegate || sender->getTag() != 12102025) return;
 		this->rotateObjectBy2656505118(false);
 	}
 	void onCCW26Point56505118(CCObject* sender) {
-		if (!sender || !m_delegate) return;
-		log::info("onCCW26Point56505118");
+		if (!sender || !m_delegate || sender->getTag() != 12102025) return;
 		this->rotateObjectBy2656505118(true);
 	}
 	void rotateObjectBy2656505118(const bool isCCW) {
@@ -53,19 +51,21 @@ class $modify(MyGJRotationControl, GJRotationControl) {
 
 		const std::string& determinedSpriteFrameName = Loader::get()->isModLoaded("hjfod.betteredit") ? "hjfod.betteredit/button-empty.png" : "geode.loader/black-square.png";
 
-		ButtonSprite* buttonSpriteCW = ButtonSprite::create(fmt::format("+{}", determinedSpriteText).c_str(), "bigFont.fnt", determinedSpriteFrameName.c_str(), .8f);
-        buttonSpriteCW->setScale(.65f);
-		buttonSpriteCW->setTag(12102025);
-		buttonSpriteCW->setID("26-point-56505118-clockwise"_spr);
-		CCMenuItemSpriteExtra* buttonCW = CCMenuItemSpriteExtra::create(buttonSpriteCW, this, menu_selector(MyGJRotationControl::onCW26Point56505118));
-		menu->addChild(buttonCW);
-
 		ButtonSprite* buttonSpriteCCW = ButtonSprite::create(fmt::format("-{}", determinedSpriteText).c_str(), "bigFont.fnt", determinedSpriteFrameName.c_str(), .8f);
         buttonSpriteCCW->setScale(.65f);
-		buttonSpriteCCW->setTag(12102025);
-		buttonSpriteCCW->setID("26-point-56505118-counterclockwise"_spr);
+		buttonSpriteCCW->setID("26-point-56505118-counterclockwise-sprite"_spr);
 		CCMenuItemSpriteExtra* buttonCCW = CCMenuItemSpriteExtra::create(buttonSpriteCCW, this, menu_selector(MyGJRotationControl::onCCW26Point56505118));
+		buttonCCW->setTag(12102025);
+		buttonCCW->setID("26-point-56505118-counterclockwise"_spr);
 		menu->addChild(buttonCCW);
+
+		ButtonSprite* buttonSpriteCW = ButtonSprite::create(fmt::format("+{}", determinedSpriteText).c_str(), "bigFont.fnt", determinedSpriteFrameName.c_str(), .8f);
+        buttonSpriteCW->setScale(.65f);
+		buttonSpriteCW->setID("26-point-56505118-clockwise-sprite"_spr);
+		CCMenuItemSpriteExtra* buttonCW = CCMenuItemSpriteExtra::create(buttonSpriteCW, this, menu_selector(MyGJRotationControl::onCW26Point56505118));
+		buttonCW->setTag(12102025);
+		buttonCW->setID("26-point-56505118-clockwise"_spr);
+		menu->addChild(buttonCW);
 
 		menu->setLayout(RowLayout::create());
 		this->addChild(menu);
